43982406e443192cf48dcbfa7be0708cbe125dc7
Tallerik <erik.klasse@gmail.com>
2018-12-02 20:40:44 +0100

    Added FancyOre Texture/Animation

--- src/main/java/mayus/mymod/MyMod.java
+++ src/main/java/mayus/mymod/MyMod.java
@@ -22,7 +22,7 @@ public class MyMod {
 
     public static CreativeTabs creativeTab = new CreativeTabs("mymod") {
         @Override
-        public ItemStack getTabIconItem() {
+        public ItemStack createIcon() {
             return new ItemStack(ModBlocks.blockFastFurnace);
         }
     };
